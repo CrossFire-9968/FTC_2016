fb1a9b11ad8b2492f271c60d2f8dc9ae3c31403e
Ryley Hindman <cessnageek@gmail.com>
2016-10-06 16:25:46 -0500

    Much simpler than the default OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CF_Vuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CF_Vuforia.java
@@ -1,13 +1,67 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 /**
  * Created by Ryley on 10/5/16.
  */
+@TeleOp(name="CF_Vuforia", group ="Test")
+//@Disabled
 public class CF_Vuforia extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        // Makes camera output appear on screen
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        // Sets camera direction
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        // License key
+        params.vuforiaLicenseKey = "AU2Vspr/////AAAAGSWZlF6AQEHFh9mbNlt5KlFGl/PX8qeeKea7jh5Xk8Ei573/nsoAjsJu9Cbi2MlRCuEIkZHQJoDGAxXmNgioA+0+DbRC6mG+1QbBu8ACMw0pBk6x3h+wvvqDeyZmjV0Fdji5Bk2bV3AaZ0AanljM2nuosjfFYOeUsoFqjE0+MQfJCOoG2ED2hxhJM88dhMaAH45kQqJ99Pn9c/F8whHUkRLeh71wW3O8qGdHEieX7WQO86VfVadHTrg0Ut8ALwiU/qVqB9pJPn+oVe9rYCixcJztb7XOp4T4Mo0IPUwVtkTUZtZTW1mAOPdbbWx3RX1OohA6q6BBU7ozDdQ1W33/L/mdETevYMf7rKPrb82Zbw8r";
+        // Sets the thing you see on screen.  Could be AXES, TEAPOT, BUILDINGS, OR NONE
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+
+        VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
+        // Lets VuForia see more than one thing at a time
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+        VuforiaTrackables beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+        beacons.get(0).setName("Wheels");
+        beacons.get(1).setName("Tools");
+        beacons.get(2).setName("Legos");
+        beacons.get(3).setName("Gears");
+
+        waitForStart();
+
+        // Activate tracking
+        beacons.activate();
+
+        while (opModeIsActive()) {
+            for(VuforiaTrackable beac : beacons) {
+                // Get position matrix, pose
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getRawPose();
+
+                if(pose != null) {
+                    VectorF translation = pose.getTranslation();
+
+                    telemetry.addData(beac.getName() + "Translation", translation);
+
+                }
+                telemetry.update();
+
+            }
+        }
+
 
     }
 }
