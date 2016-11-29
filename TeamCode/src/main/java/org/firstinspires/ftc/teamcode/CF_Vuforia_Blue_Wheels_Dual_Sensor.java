a62bc8d0f49aa89dea731e46db0d8c2d5a7cf5c9
Ryley Hindman <cessnageek@gmail.com>
2016-11-28 23:33:51 -0600

    This code now reliably hits two beacons, for the blue side.  The red side still has some kinks to work out, so I’ll save that for a later date.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CF_Vuforia_Blue_Wheels_Dual_Sensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CF_Vuforia_Blue_Wheels_Dual_Sensor.java
@@ -38,44 +38,58 @@ import java.util.concurrent.TimeUnit;
  */
 @Autonomous(name="CF_Vuforia_Blue_Wheels_Dual_Sensor", group ="Blue")
 //@Disabled
-public class CF_Vuforia_Blue_Wheels_Dual_Sensor extends CF_Library implements SensorEventListener{
+public class CF_Vuforia_Blue_Wheels_Dual_Sensor extends CF_Library implements SensorEventListener {
 
     float xAccel = 0;
     float yAccel = 0;
     float zAccel = 0;
+    OpenGLMatrix pose = null;
+
+    final int stopCount = 110;
+    boolean seeableFirst;
+    boolean seeableSecond;
+    int x;
+    int y;
+    int z;
+    int error;
+
+
+    double kP = 0.0005;
+    double power = 0.2;
+    double effort;
+    double leftPower;
+    double rightPower;
+    final int FIRSTPICTURE = 0;
+    final int SECONDPICTURE = 2;
+    int encoderCounts = 0;
+    int picFlag = 0;
+    int beaconFlagFirst = 0;
+
+    ColorSensor sensorRGBright;
+    ColorSensor sensorRGBleft;
 
     private SensorManager sensorManager;
     private Sensor accelSen;
 
+
+    float hsvValuesright[] = {0F, 0F, 0F};
+    float hsvValuesleft[] = {0F, 0F, 0F};
+
     @Override
-    public void runOpMode ()throws InterruptedException {
+    public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
         robot.MotorMecanumLeftFront.setDirection(DcMotor.Direction.FORWARD);     // Set to REVERSE if using AndyMark motors
         robot.MotorMecanumLeftRear.setDirection(DcMotor.Direction.FORWARD);      // Set to REVERSE if using AndyMark motors
         robot.MotorMecanumRightFront.setDirection(DcMotor.Direction.REVERSE);    // Set to FORWARD if using AndyMark motors
         robot.MotorMecanumRightRear.setDirection(DcMotor.Direction.REVERSE);
-        int x;
-        int y;
-        int z;
-        int error;
-        double kP = 0.0005;
-        double power = 0.2;
-        double effort;
-        double leftPower;
-        double rightPower;
-        boolean seeable;
+
+
         int firstFlag = 0;
-        int picFlag = 0;
-        int turnFlag = 0;
-        int beaconFlag = 0;
-        final int RedUpperLimit_LowRange = 20;
-        final int RedLowerLimit_LowRange = 0;
-        final int RedUpperLimit_highRange = 360;
-        final int RedLowerLimit_highRange = 325;
-        final int BlueUpperLimit = 270;
-        final int BlueLowerLimit = 0;
-        final int stopCount = 110;
-        int encoderCounts = 0;
+
+        int turnFlagFirst = 0;
+        int turnFlagSecond = 0;
+
+
 
         int countRight = 1;
         int countLeft = 1;
@@ -83,14 +97,13 @@ public class CF_Vuforia_Blue_Wheels_Dual_Sensor extends CF_Library implements Se
         final int LEFT = 0;
         final int RIGHT = 1;
 
-        ColorSensor sensorRGBright;
-        ColorSensor sensorRGBleft;
+
         DeviceInterfaceModule cdimright;
         DeviceInterfaceModule cdimleft;
 
 
         final int LED_CHANNEL = 5;
-        final int PICTURE = 3;
+
 
         // Makes camera output appear on screen
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
@@ -111,8 +124,6 @@ public class CF_Vuforia_Blue_Wheels_Dual_Sensor extends CF_Library implements Se
         beacons.get(2).setName("Legos");
         beacons.get(3).setName("Gears");
 
-        float hsvValuesright[] = {0F, 0F, 0F};
-        float hsvValuesleft[] = {0F, 0F, 0F};
 
         cdimright = hardwareMap.deviceInterfaceModule.get("CF_DimRight");
 
@@ -130,7 +141,7 @@ public class CF_Vuforia_Blue_Wheels_Dual_Sensor extends CF_Library implements Se
 
         //Attach Accel listner
         sensorManager.registerListener(this, accelSen, SensorManager.SENSOR_DELAY_NORMAL);
-        OpenGLMatrix pose = null;
+
         telemetry.addData("xAccel", xAccel);
         telemetry.clearAll();
         telemetry.update();
@@ -140,137 +151,52 @@ public class CF_Vuforia_Blue_Wheels_Dual_Sensor extends CF_Library implements Se
 
         while (opModeIsActive()) {
             if (firstFlag == 0) {
-                this.encoderStrafeLeft(3000, 0.3f);
+                this.encoderStrafeLeft(3000, 0.7f);
                 this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                this.encoderMove(1500, 1500, 0.3f, 0.3f);
+                this.encoderMove(1500, 1500, 0.7f, 0.7f);
                 this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                this.encoderStrafeLeft(1900, 0.3f);
-                TimeUnit.SECONDS.sleep(0.5);
+                this.encoderStrafeLeft(1900, 0.7f);
+                TimeUnit.SECONDS.sleep((long)0.7);
                 firstFlag = 1;
             }
-            seeable = ((VuforiaTrackableDefaultListener) beacons.get(PICTURE).getListener()).isVisible();
-            int leftCount = robot.MotorMecanumLeftFront.getCurrentPosition();
-            int rightCount = robot.MotorMecanumRightFront.getCurrentPosition();
-            while (!seeable && !isStopRequested() && turnFlag == 0) {
+            seeableFirst = ((VuforiaTrackableDefaultListener) beacons.get(FIRSTPICTURE).getListener()).isVisible();
+            while (!seeableFirst && !isStopRequested() && turnFlagFirst == 0) {
                 this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                this.encoderStrafeLeft(100, 0.3f);
-                seeable = ((VuforiaTrackableDefaultListener) beacons.get(PICTURE).getListener()).isVisible();
+                this.encoderStrafeLeft(100, 0.7f);
+                seeableFirst = ((VuforiaTrackableDefaultListener) beacons.get(FIRSTPICTURE).getListener()).isVisible();
             }
 
-            if (seeable) {
+            if (seeableFirst) {
                 setPower(0.0f);
-                turnFlag = 1;
-            }
-            pose = ((VuforiaTrackableDefaultListener) beacons.get(PICTURE).getListener()).getRawPose();
-
-
-            // NOTE: If picture not seen is pose = NULL?
-            // Is "visible" on screen with light?
-            while(pose != null && picFlag == 0 && !isStopRequested()) {
-                if(isStopRequested()) {
-                    requestOpModeStop();
-                }
-                VectorF translation = pose.getTranslation();
-                x = (int) translation.get(2);
-                telemetry.clearAll();
-                telemetry.addData("visible", "visible");
-                telemetry.addData("xValue: ", x);
-                telemetry.update();
-
-                //x, y, and z position
-                VectorF xPose = pose.getRow(2);
-                VectorF yPose = pose.getRow(1);
-                VectorF zPose = pose.getRow(0);
-
-                double XPose = (double)xPose.get(0);
-                double cosXPose = Math.toDegrees(Math.acos(XPose));
-
-                // Get the x, y, and z components, and cast them to ints, because we don't need the full
-                // double precision
-                z = (int)translation.get(0); //Switch y & z for landscape mode
-                y = (int)translation.get(1);
-                x = (int)translation.get(2);
-
-                telemetry.addData("x: ", x/25.4);
-                telemetry.addData("y: ", y/25.4);
-                telemetry.addData("z: ", z/25.4);
-
-                telemetry.addData("xPose: ", xPose);
-                telemetry.addData("yPose: ", yPose);
-                telemetry.addData("zPose: ", zPose);
-                telemetry.addData("cosXPose: ", cosXPose);
-                seeable = ((VuforiaTrackableDefaultListener) beacons.get(PICTURE).getListener()).isVisible();
-
-                while (x >= stopCount && !isStopRequested() && seeable && encoderCounts < 5000) {
-                    pose = ((VuforiaTrackableDefaultListener) beacons.get(PICTURE).getListener()).getRawPose();
-                    seeable = ((VuforiaTrackableDefaultListener) beacons.get(PICTURE).getListener()).isVisible();
-                    encoderCounts = robot.MotorMecanumLeftFront.getCurrentPosition();
-                    if (pose != null) {
-                        translation = pose.getTranslation();
-                        y = (int) translation.get(1);
-                        x = (int) translation.get(2);
-                        telemetry.addData("xPose: ", x);
-                        telemetry.addData("y: ", y);
-                        telemetry.update();
-                        error = y + 10;
-                        effort = kP * error;
-                        rightPower = power + effort;
-                        leftPower = power - effort;
-                        robot.MotorMecanumLeftFront.setPower(leftPower);
-                        robot.MotorMecanumRightFront.setPower(rightPower);
-                        robot.MotorMecanumLeftRear.setPower(leftPower);
-                        robot.MotorMecanumRightRear.setPower(rightPower);
-                    }
-                }
-                if ((x < stopCount && !isStopRequested()) || !seeable || encoderCounts >= 5000) {
-                    robot.MotorMecanumLeftFront.setPower(0.0f);
-                    robot.MotorMecanumRightFront.setPower(0.0f);
-                    robot.MotorMecanumLeftRear.setPower(0.0f);
-                    robot.MotorMecanumRightRear.setPower(0.0f);
-                    if(x < stopCount && !isStopRequested()) {
-                        picFlag = 1;
-                    }
-                }
-                pose = ((VuforiaTrackableDefaultListener) beacons.get(PICTURE).getListener()).getRawPose();
-
-
+                turnFlagFirst = 1;
             }
 
-            //Change Here
-            if(beaconFlag == 0) {
-                Color.RGBToHSV((sensorRGBright.red() * 255) / 800, (sensorRGBright.green() * 255) / 800, (sensorRGBright.blue() * 255) / 800, hsvValuesright);
-                Color.RGBToHSV((sensorRGBleft.red() * 255) / 800, (sensorRGBleft.green() * 255) / 800, (sensorRGBleft.blue() * 255) / 800, hsvValuesleft);
-                float hueRight = hsvValuesright[0];
-                if (sensorRGBright.blue() > sensorRGBright.red() && sensorRGBleft.red() > sensorRGBleft.blue()) {
-                    telemetry.addData("blue", hsvValuesright[0]);
-                    telemetry.update();
-                    robot.SetButtonPusherPosition(0.00);
-                    this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    TimeUnit.SECONDS.sleep(1);
-                    this.encoderMove(300, 300, 0.2f, 0.2f);
-                    beaconFlag = 1;
-                    TimeUnit.SECONDS.sleep(1);
-                    this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    this.encoderMove(-300, -300, 0.2f, 0.2f);
-                    requestOpModeStop();
-                } else if (sensorRGBright.red() > sensorRGBright.blue() && sensorRGBleft.blue() > sensorRGBleft.red())
-                {
-                    telemetry.addData("red", hsvValuesright[0]);
-                    telemetry.update();
-                    robot.SetButtonPusherPosition(0.90);
-                    this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    TimeUnit.SECONDS.sleep(1);
-                    this.encoderMove(300, 300, 0.2f, 0.2f);
-                    beaconFlag = 1;
-                    TimeUnit.SECONDS.sleep(1);
-                    this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    this.encoderMove(-300, -300, 0.2f, 0.2f);
-                    requestOpModeStop();
-                } else {
-                    telemetry.addData("unknown", hsvValuesright[0]);
-                    telemetry.update();
-                }
+            driveToBeacon(FIRSTPICTURE, beacons);
+
+            pushBeaconButton();
+
+            this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            this.encoderStrafeLeft(4500, 0.7f);
+
+            seeableSecond = ((VuforiaTrackableDefaultListener) beacons.get(SECONDPICTURE).getListener()).isVisible();
+            while (!seeableSecond && !isStopRequested() && turnFlagSecond == 0) {
+                this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                this.encoderStrafeLeft(100, 0.7f);
+                seeableSecond = ((VuforiaTrackableDefaultListener) beacons.get(SECONDPICTURE).getListener()).isVisible();
             }
+
+            if (seeableSecond) {
+                setPower(0.0f);
+                turnFlagSecond = 1;
+            }
+
+            picFlag = 0;
+            driveToBeacon(SECONDPICTURE, beacons);
+
+            pushBeaconButton();
+
+            requestOpModeStop();
 
 
         }
@@ -316,5 +242,147 @@ public class CF_Vuforia_Blue_Wheels_Dual_Sensor extends CF_Library implements Se
         yAccel = event.values[1];
         zAccel = event.values[2];
     }
+
+    public void driveToBeacon(int pictureNumber, VuforiaTrackables beaconsArray) {
+        if (isStopRequested()) {
+            requestOpModeStop();
+        }
+        robot.SetButtonPusherPosition(0.45f);
+        pose = ((VuforiaTrackableDefaultListener) beaconsArray.get(pictureNumber).getListener()).getRawPose();
+
+        if(pose != null) {
+            VectorF translation = pose.getTranslation();
+            x = (int) translation.get(2);
+            telemetry.clearAll();
+            telemetry.addData("visible", "visible");
+            telemetry.addData("xValue: ", x);
+            telemetry.update();
+
+            //x, y, and z position
+            VectorF xPose = pose.getRow(2);
+            VectorF yPose = pose.getRow(1);
+            VectorF zPose = pose.getRow(0);
+
+            double XPose = (double) xPose.get(0);
+            double cosXPose = Math.toDegrees(Math.acos(XPose));
+
+            // Get the x, y, and z components, and cast them to ints, because we don't need the full
+            // double precision
+            z = (int) translation.get(0); //Switch y & z for landscape mode
+            y = (int) translation.get(1);
+            x = (int) translation.get(2);
+
+            telemetry.addData("x: ", x / 25.4);
+            telemetry.addData("y: ", y / 25.4);
+            telemetry.addData("z: ", z / 25.4);
+
+            telemetry.addData("xPose: ", xPose);
+            telemetry.addData("yPose: ", yPose);
+            telemetry.addData("zPose: ", zPose);
+            telemetry.addData("cosXPose: ", cosXPose);
+            seeableFirst = ((VuforiaTrackableDefaultListener) beaconsArray.get(pictureNumber).getListener()).isVisible();
+        }
+
+        while (x >= stopCount && !isStopRequested() && seeableFirst && encoderCounts < 5000) {
+            pose = ((VuforiaTrackableDefaultListener) beaconsArray.get(pictureNumber).getListener()).getRawPose();
+
+
+            // NOTE: If picture not seen is pose = NULL?
+            // Is "visible" on screen with light?
+            while (pose != null && picFlag == 0 && !isStopRequested()) {
+                if (isStopRequested()) {
+                    requestOpModeStop();
+                }
+                VectorF translation = pose.getTranslation();
+                x = (int) translation.get(2);
+                telemetry.clearAll();
+                telemetry.addData("visible", "visible");
+                telemetry.addData("xValue: ", x);
+                telemetry.update();
+
+                // Get the x, y, and z components, and cast them to ints, because we don't need the full
+                // double precision
+                z = (int) translation.get(0); //Switch y & z for landscape mode
+                y = (int) translation.get(1);
+                x = (int) translation.get(2);
+
+                telemetry.addData("x: ", x / 25.4);
+                telemetry.addData("y: ", y / 25.4);
+                telemetry.addData("z: ", z / 25.4);
+
+                seeableFirst = ((VuforiaTrackableDefaultListener) beaconsArray.get(pictureNumber).getListener()).isVisible();
+
+                while (x >= stopCount && !isStopRequested() && seeableFirst && encoderCounts < 5000) {
+                    pose = ((VuforiaTrackableDefaultListener) beaconsArray.get(pictureNumber).getListener()).getRawPose();
+                    seeableFirst = ((VuforiaTrackableDefaultListener) beaconsArray.get(pictureNumber).getListener()).isVisible();
+                    encoderCounts = robot.MotorMecanumLeftFront.getCurrentPosition();
+                    if (pose != null) {
+                        translation = pose.getTranslation();
+                        y = (int) translation.get(1);
+                        x = (int) translation.get(2);
+                        telemetry.addData("xPose: ", x);
+                        telemetry.addData("y: ", y);
+                        telemetry.update();
+                        error = y + 10;
+                        effort = kP * error;
+                        rightPower = power + effort;
+                        leftPower = power - effort;
+                        robot.MotorMecanumLeftFront.setPower(leftPower);
+                        robot.MotorMecanumRightFront.setPower(rightPower);
+                        robot.MotorMecanumLeftRear.setPower(leftPower);
+                        robot.MotorMecanumRightRear.setPower(rightPower);
+                    }
+                }
+                if ((x < stopCount && !isStopRequested()) || !seeableFirst || encoderCounts >= 5000) {
+                    robot.MotorMecanumLeftFront.setPower(0.0f);
+                    robot.MotorMecanumRightFront.setPower(0.0f);
+                    robot.MotorMecanumLeftRear.setPower(0.0f);
+                    robot.MotorMecanumRightRear.setPower(0.0f);
+                    if (x < stopCount && !isStopRequested()) {
+                        picFlag = 1;
+                    }
+                }
+                pose = ((VuforiaTrackableDefaultListener) beaconsArray.get(pictureNumber).getListener()).getRawPose();
+
+            }
+        }
+    }
+
+    public void pushBeaconButton() throws InterruptedException{
+        //if (beaconFlagFirst == 0) {
+            Color.RGBToHSV((sensorRGBright.red() * 255) / 800, (sensorRGBright.green() * 255) / 800, (sensorRGBright.blue() * 255) / 800, hsvValuesright);
+            Color.RGBToHSV((sensorRGBleft.red() * 255) / 800, (sensorRGBleft.green() * 255) / 800, (sensorRGBleft.blue() * 255) / 800, hsvValuesleft);
+            if (sensorRGBright.blue() > sensorRGBright.red() && sensorRGBleft.red() > sensorRGBleft.blue()) {
+                telemetry.addData("blue", hsvValuesright[0]);
+                telemetry.update();
+                robot.SetButtonPusherPosition(0.00);
+                this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                TimeUnit.SECONDS.sleep(1);
+                this.encoderMove(300, 300, 0.2f, 0.2f);
+                beaconFlagFirst = 1;
+                TimeUnit.SECONDS.sleep(1);
+                this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                this.encoderMove(-1500, -1500, 0.2f, 0.2f);
+                //requestOpModeStop();
+            } else if (sensorRGBright.red() > sensorRGBright.blue() && sensorRGBleft.blue() > sensorRGBleft.red()) {
+                telemetry.addData("red", hsvValuesright[0]);
+                telemetry.update();
+                robot.SetButtonPusherPosition(0.90);
+                this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                TimeUnit.SECONDS.sleep(1);
+                this.encoderMove(300, 300, 0.2f, 0.2f);
+                beaconFlagFirst = 1;
+                TimeUnit.SECONDS.sleep(1);
+                this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                this.encoderMove(-1500, -1500, 0.2f, 0.2f);
+                //requestOpModeStop();
+            } else {
+                telemetry.addData("unknown", hsvValuesright[0]);
+                telemetry.update();
+            }
+        //}
+    }
 }
+
+
 
