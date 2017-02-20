b4be75f182c7e016ca8b811aa9edf90eda27d858
DESKTOP-88UN80K\dawso <dawsonlaurene@gmail.com>
2017-02-20 16:20:06 -0600

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CF_BackupAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CF_BackupAuto.java
@@ -20,114 +20,66 @@ public class CF_BackupAuto extends LinearOpMode
 
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
+    private enum states
+    {
+        AUTOINIT, DRIVETOPOSITION, STARTBALLSHOOTER, STOPBALLSHOOTER, DRIVETOBALL, AUTOCOMPLETE, END
+    }
 
     @Override
     public void runOpMode() throws InterruptedException
     {
         // Wait for the game to start (driver presses PLAY)
+        states State = states.AUTOINIT;
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
-        if (opModeIsActive())
+        while (opModeIsActive())
         {
             // Initialization routines
-            autoInit();
+            switch (State)
+            {
+                case AUTOINIT:
+                    robot.init(hardwareMap);
+                    robot.Loader.setPosition(0.08);
+                    State = states.DRIVETOPOSITION;
+                    break;
+                case DRIVETOPOSITION:
+                    robot.setMecanumEncoderMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    robot.setMecanumEncoderMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.setMecanumEncoderTargetPosition(2200, 2200, 2200, 2200);
+                    robot.setMecanumPowers(0.4, 0.4, 0.4, 0.4);
+                    State = states.STARTBALLSHOOTER;
+                    break;
+                case STARTBALLSHOOTER:
+                    robot.setShooterEncoderMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    robot.Shooter.setPower(-0.45f);
+                    TimeUnit.SECONDS.sleep(2);
+                    robot.SetLoaderPosition(0.0);
+                    TimeUnit.SECONDS.sleep(4);
+                    State = states.STOPBALLSHOOTER;
+                    break;
+                case STOPBALLSHOOTER:
+                    robot.Shooter.setPower(0.0f);
+                    State = states.DRIVETOBALL;
+                    break;
+                case DRIVETOBALL:
+                    robot.setMecanumEncoderMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    robot.setMecanumEncoderMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.setMecanumEncoderTargetPosition(800, 800, 800, 800);
+                    robot.setMecanumPowers(0.4, 0.4, 0.4, 0.4);
+                    State = states.END;
+                    break;
+                case END:
+                    // Set motors to run by encoders and turn off power
+                    robot.setMecanumPowers(0.0, 0.0, 0.0, 0.0);
+                    robot.Shooter.setPower(0.0);
 
-            driveToPosition();
-
-            startBallShooter();
-
-            stopBallShooter();
-
-            driveToBall();
-
-            Park();
-
-            autoComplete();
-
-            requestOpModeStop();
+                    telemetry.clear();
+                    telemetry.addData("State: ", "Auto Done");
+                    telemetry.update();
+                    requestOpModeStop();
+            }
         }
-    }
-
-
-    /***
-     * Initialization routine.  Nothing much happens here except updating
-     * the drivers station that we reached this point.  As other actuators
-     * are added, the their initializations would happen here
-     * (e.g. initial servo position)
-     */
-
-    private void autoInit()
-    {
-        robot.init(hardwareMap);
-        robot.Loader.setPosition(0.08);
-        telemetry.clear();
-        telemetry.clear();
-        telemetry.addData("State: ", "init");
-        telemetry.update();
-    }
-
-    private void driveToPosition()
-    {
-        robot.setMecanumEncoderMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.setMecanumEncoderMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.setMecanumEncoderMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.setMecanumEncoderTargetPosition(2000, 2000, 2000, 2000);
-        robot.setMecanumPowers(0.4, 0.4, 0.4, 0.4);
-    }
-
-    private void startBallShooter() throws InterruptedException
-    {
-        robot.setShooterEncoderMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.Shooter.setPower(-0.45f);
-        TimeUnit.SECONDS.sleep(2);
-        robot.SetLoaderPosition(0.0);
-        TimeUnit.SECONDS.sleep(4);
-        telemetry.clear();
-        telemetry.addData("State: ", "Ball Shooter");
-        telemetry.update();
-    }
-
-    private void stopBallShooter()
-    {
-        robot.Shooter.setPower(0.0f);
-        telemetry.clear();
-        telemetry.addData("State: ", "Stop Ball Shooter");
-        telemetry.update();
-    }
-
-    private void driveToBall()
-    {
-        robot.setMecanumEncoderMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.setMecanumEncoderMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.setMecanumEncoderTargetPosition(300, 300, 300, 300);
-        robot.setMecanumPowers(0.4, 0.4, 0.4, 0.4);
-    }
-
-    private void Park()
-    {
-        robot.setMecanumEncoderMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.setMecanumEncoderMode(DcMotor.RunMode.RUN_TO_POSITION);
-        telemetry.clear();
-        telemetry.addData("State: ", "Park");
-        robot.setMecanumPowers(0, 0, 0, 0);
-    }
-
-    /***
-     * This is the final stage of autonomous.  Once entered, the robot will
-     * happily wait until the end and do nothing.  This method can be used
-     * to stop the robot if something goes awry or it can be the ending point
-     * once all other tasks are complete.
-     */
-    private void autoComplete()
-    {
-        // Set motors to run by encoders and turn off power
-        robot.setMecanumPowers(0.0, 0.0, 0.0, 0.0);
-        robot.Shooter.setPower(0.0);
-
-        telemetry.clear();
-        telemetry.addData("State: ", "Auto Done");
-        telemetry.update();
     }
 }
